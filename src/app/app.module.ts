0ee87133b89b94c1c07bcf843e8017f086a26f15
Govind Singh <govind.resarch0000@rediffmail.com>
2019-03-14 13:32:15 +0530

    google login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './login/account/account.component';
+import { LoginModule } from './login/login.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule, 
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
